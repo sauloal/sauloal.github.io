30afdc5b74bf5e2fc870940c7922a9484a3c2627
Saulo Alves <sauloal@gmail.com>
2015-02-25 00:05:09 +0100

    smaller json

--- _data/repos/get_github_repos.sh
+++ _data/repos/get_github_repos.sh
@@ -1,1 +1,2 @@
-curl https://api.github.com/users/sauloal/repos > repos/github.json
+#curl https://api.github.com/users/sauloal/repos | grep -v -e "https://api" -e "https://avatars" > github.json
+curl https://api.github.com/users/sauloal/repos | grep -e "{" -e "}" -e "full_name" -e "html_url" -e "\"description\"" | grep -v -e "https://api" -e "https://avatars" > github.json
